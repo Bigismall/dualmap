3725b05723a5fe86892daadb5a095b8cbb630bc3
Bigismall <rondelious@gmail.com>
2024-11-11 08:44:40 +0100

    fix biome

--- src/layers.ts
+++ src/layers.ts
@@ -38,10 +38,14 @@ export const osmLayers: Record<LayerName, L.TileLayer[]> = {
     }),
   ],
   Rail: [
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-    }),
+    L.tileLayer(
+      'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+      {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+        maxZoom: 16,
+      },
+    ),
+
     L.tileLayer('https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png', {
       attribution:
         'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map style: &copy; <a href="https://www.OpenRailwayMap.org">OpenRailwayMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import biomePlugin from 'vite-plugin-biome';
 
 export default {
-    base: '/dualmap/',
-    plugins: [biomePlugin({
-        mode: 'check',
-        files: 'src/*.ts',
-        applyFixes: true
-    })],
+  base: '/dualmap/',
+  plugins: [
+    biomePlugin({
+      mode: 'check',
+      files: '.',
+      applyFixes: true,
+    }),
+  ],
 };
