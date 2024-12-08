f1065f0f117a2fe1f6c7201970e0a34aec012c8c
Sławomir Rodak <slawomir.rodak@schibsted.pl>
2024-12-08 12:19:03 +0100

    fix: Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-import { resolve } from 'path';
 import biomePlugin from 'vite-plugin-biome';
 
 export default {
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        osmbuildings: resolve(__dirname, '/osmbuildings/index.html'),
-      },
-    },
-  },
-
   base: '/dualmap/',
-
   plugins: [biomePlugin()],
 };
